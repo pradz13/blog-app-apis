01cf5c8fef8cbe310352a7c0a27847a31b5c8a62
Pradipta <pradipta.nag2@gmail.com>
2022-05-22 08:11:48 +0530

    Update to Post Entity

--- src/main/java/com/app/blog/entity/Post.java
+++ src/main/java/com/app/blog/entity/Post.java
@@ -10,8 +10,15 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
 @Table(name = "posts")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Post {
 
     @Id
